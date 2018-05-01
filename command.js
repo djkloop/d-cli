5b6f7399b250e347f0f2bd0e2602a6eb37f99187
zhangjianlei <zhangjianlei@appicplay.com>
2018-05-01 18:09:40 +0800

    update --help && -i,-d,-b,-s,-p command

--- command.js
+++ command.js
@@ -2,4 +2,50 @@
 
 const commander = require('commander');
 const chalk = require('chalk');
+const fs = require('fs');
+const path = require('path');
+
+const version = require('./package.json').version
+
+ /** 
+  * --help log info
+ */
+
+commander.on('--help', function() {
+    console.log('  Example:')
+    console.log();
+    console.log(chalk.gray('    # create a new project with an official template'));
+    console.log('   $ d-cli --init or d-cli -i');
+    console.log();
+    console.log(chalk.gray('    # development project'));
+    console.log('   $ d-cli --dev or d-cli -d');
+    console.log()
+    console.log(chalk.gray('    # start local server'))
+    console.log('   $ d-cli --server or d-cli -s')
+    console.log()
+ });
+
+
+commander
+    .version(version)
+    .description(chalk.green(
+        'd-cli for nodejs \n' +
+        chalk.bold('    this cli just learn. \n') +
+        '       d-cli now version is '+ version +''
+    ))
+    .usage(chalk.bold('[options]'))
+    .option('-i, --init', 'init a new project for a template')
+    .option('-d, --dev', 'start project for development')
+    .option('-b, --build', 'start project for production')
+    .option('-s, --server', 'local server')
+    .option('-p, --publish', 'npm publish')
+    .parse(process.argv)
+
+/**
+ * init
+ */
+
+ if(commander.init) {
+     require('./src/task/task-init.js').render();
+ }
 
---
+++ src/task/task-init.js
@@ -0,0 +1,5 @@
+module.exports = {
+    render: () => {
+        console.log('我是render方法...')
+    }
+}
